799389d55ccee5cc96d083e86a2493c9eccd5c8d
dedge-ct <tuquerojedric@gmail.com>
2025-04-11 00:24:38 +0800

    feat: implemented customizable grid size

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+
+const maxCanvasHeight = 960;
+const maxCanvasWidth = 960;
+const gridSize = 50;
+
 const canvasPixelContainer = document.querySelector("#canvas-container");
-const gridSize = 16;
+canvasPixelContainer.style.maxHeight = maxCanvasHeight + "px";
+canvasPixelContainer.style.maxWidth = maxCanvasWidth + "px";
 
 
 for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
         let canvasPixel = document.createElement("div");
         canvasPixel.classList.add("canvas-pixel");
-        canvasPixel.style.backgroundColor = "red";
-        canvasPixel.style.height = "60px";
-        canvasPixel.style.width = "60px";
+        canvasPixel.style.backgroundColor = "white";
+        canvasPixel.style.height = parseInt(parseInt(canvasPixelContainer.style.maxHeight)/gridSize) + "px";
+        canvasPixel.style.width = parseInt(parseInt(canvasPixelContainer.style.maxWidth)/gridSize) + "px";
         canvasPixel.style.margin = 0;
         canvasPixelContainer.appendChild(canvasPixel);
     }
 }
+
+const canvasPixels = document.querySelectorAll(".canvas-pixel");
+canvasPixels.forEach((canvasPixel) => {
+    canvasPixel.addEventListener("mouseover", () => {
+        canvasPixel.style.backgroundColor = "black";
+    });
+});
