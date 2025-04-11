2dd5b556cc437de8b9fdec3a3a378a98a29177e5
dedge-ct <tuquerojedric@gmail.com>
2025-04-11 22:40:27 +0800

    feat: added borders to canvas

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 
 const maxCanvasHeight = 960;
 const maxCanvasWidth = 960;
-const gridSize = parseInt(prompt("Insert grid size."));
+const gridSize = 16;
 
 const canvasPixelContainer = document.querySelector("#canvas-container");
 canvasPixelContainer.style.maxHeight = maxCanvasHeight + "px";
 canvasPixelContainer.style.maxWidth = maxCanvasWidth + "px";
 
-
+//Create canvas for specified amount of pixels
 for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
         let canvasPixel = document.createElement("div");
         canvasPixel.classList.add("canvas-pixel");
         canvasPixel.style.backgroundColor = "white";
-        canvasPixel.style.height = parseInt(parseInt(canvasPixelContainer.style.maxHeight)/gridSize) + "px";
-        canvasPixel.style.width = parseInt(parseInt(canvasPixelContainer.style.maxWidth)/gridSize) + "px";
+        canvasPixel.style.height = parseInt(maxCanvasHeight/gridSize) - 2 + "px";
+        console.log(parseInt(maxCanvasHeight/gridSize) - 2);
+        canvasPixel.style.width = parseInt(maxCanvasWidth/gridSize) - 2 + "px";
+        canvasPixel.style.border = "1px solid gray";
         canvasPixel.style.margin = 0;
         canvasPixelContainer.appendChild(canvasPixel);
     }
 }
-//Adjusts height of canvas to perfectly fit number of pixels specified
-canvasPixelContainer.style.height = parseInt(document.querySelector(".canvas-pixel").style.height) * gridSize + "px";
-canvasPixelContainer.style.width = parseInt(document.querySelector(".canvas-pixel").style.width) * gridSize + "px";
-
+//Adjusts size of canvas to perfectly fit number of pixels specified
+canvasPixelContainer.style.height = (parseInt(document.querySelector(".canvas-pixel").style.height) + 2) * gridSize + "px";
+canvasPixelContainer.style.width = (parseInt(document.querySelector(".canvas-pixel").style.width) + 2) * gridSize + "px";
 const canvasPixels = document.querySelectorAll(".canvas-pixel");
 canvasPixels.forEach((canvasPixel) => {
     canvasPixel.addEventListener("mouseover", () => {
